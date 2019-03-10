67be45797770095982d4a512b0dd8c39fa00d4e7
Kristoffer Lerbæk Pedersen <github@lerbaek.dk>
2019-03-10 22:14:09 +0100

    Add library to get shopping list from Mad For Fattigrøve

---
+++ MadForFattigroeve.Test/ShoppingListTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MadForFattigroeve.Test
+{
+  public class ShoppingListTests
+  {
+    private ShoppingList _uut;
+
+    private readonly KeyValuePair<string, IEnumerable<string>> _expectedShoppingList
+      = new KeyValuePair<string, IEnumerable<string>>(
+          "https://madforfattigroeve.dk/2019/03/2019-madplan-for-uge-11/", new[]
+    {
+      "1 kg. gulerødder = 6,50 kr.",
+      "2 x squash = 10 kr. (Tilbud)",
+      "2 kg. kartofler, løsvægt = 15 kr.",
+      "2 x broccoli = 16 kr. (Tilbud)",
+      "Frisk spinat, 75 g. = 9 kr.",
+      "Schulstad rugbrød = 12 kr.",
+      "Baguettes = 3,95 kr.",
+      "Æg = 23,50 kr.",
+      "Revet cheddar = 11,95 kr.",
+      "2 x bacon i skiver = 17,90 kr.",
+      "1 x bacon i tern = 10,95 kr.",
+      "450 g. kyllingebrystfilet = 26,50 kr.",
+      "Hele kyllingelår, 1,5 kg. = 20 kr. (Tilbud)",
+      "450 g. hk. oksefars = 20 kr. (Tilbud)",
+      "Bønner, frost = 10,50 kr.",
+      "Garli flødeost = 8,25 kr.",
+      "Mælk = 7,50 kr.",
+      "Frisk mozzarella = 5,95 kr.",
+      "Fløde, OMA = 7,95 kr.",
+      "Kyllingebrystfilet, pålæg = 10 kr. (Tilbud)",
+      "Smør = 12,50 kr.",
+      "Ris = 4,50 kr.",
+      "Lasagneblanding = 14,95 kr.",
+      "Bearnaisesauce = 9,95 kr.",
+      "Løg + hvidløg",
+      "Salt + peber",
+      "Olie",
+      "Mel",
+      "Bagepulver",
+      "Solsikkekerner",
+      "Oksebouillon",
+      "Grøntsagsbouillon",
+      "Sukker",
+      "Rødløg",
+      "Oregano",
+      "Citronsaft",
+      "Herbes de provence",
+      "Mayonnaise"
+    });
+
+    [SetUp]
+    public void Setup() => _uut = new ShoppingList();
+
+    [Test]
+    public void DoStuff() => Assert.That(_uut.GetShoppingList(_expectedShoppingList.Key), Is.EquivalentTo(_expectedShoppingList.Value));
+  }
+}
---
+++ MadForFattigroeve/ShoppingList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace MadForFattigroeve
+{
+  public class ShoppingList
+  {
+    private static HtmlWeb HtmlWeb => new HtmlWeb();
+
+    private string NewestWeekPlanLink
+      => HtmlWeb.Load("https://madforfattigroeve.dk/category/ugeplaner")
+                .DocumentNode                                        // Document
+                .Descendants("a")                                    // Link elements
+                .First(a => a.InnerText.Contains("Madplan for uge")) // Latest plan link element
+                .GetAttributeValue("href", null);                    // Link
+
+    public IEnumerable<string> NewestShoppingList => GetShoppingList(NewestWeekPlanLink);
+
+    public IEnumerable<string> GetShoppingList(string weekPlanLink)
+      => HtmlWeb.Load(weekPlanLink)
+                .DocumentNode                            // Document
+                .Descendants("table")                    // Table elements
+                .Single(t => t.HasClass("shoppinglist")) // Table with shopping list
+                .Descendants("li")                       // List elements
+                .Select(li => li.InnerText               // Shopping list entry
+                                .TrimEnd(' ', '\r'));         // Trimmed
+  }
+}
